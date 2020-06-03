2a5507e3d4b5a8f4174e90a03889dab88b6bcb30
Tyson Mote <tyson@segment.com>
2020-06-03 15:45:07 -0700

    destinations: Make test slightly more meaningful

--- destinations/index.test.js
+++ destinations/index.test.js
@@ -3,6 +3,27 @@ const destinations = require('.')
 describe('metadata', () => {
   test('load it up!', () => {
     const all = destinations()
-    expect(typeof all).toBe('object')
+    expect(Array.isArray(all)).toBe(true)
+
+    for (const destination of all) {
+      expect(Object.keys(destination).sort()).toStrictEqual([
+        'name',
+        'slug',
+        'path',
+        'settings',
+        'defaultSubscriptions',
+        'partnerActions'
+      ].sort())
+
+      for (const action of destination.partnerActions) {
+        expect(Object.keys(action).sort()).toStrictEqual([
+          'slug',
+          'settings',
+          'mapping',
+          'schema',
+          'code'
+        ].sort())
+      }
+    }
   })
 })
