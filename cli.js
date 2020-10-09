04aa304ba23aeaa837d740ea19c42c640bbf3275
Matt Shwery <matthew.shwery@segment.com>
2020-10-08 19:35:44 -0500

    remove cli/deploy and webpack compilation (#36)
    Co-authored-by: Steven Miller <stevenmiller888@gmail.com>

--- cli.js
+++ cli.js
@@ -2,7 +2,6 @@
 
 require('yargs')
   .command(require('./src/cli/run-local'))
-  .command(require('./src/cli/deploy'))
   .command(require('./src/cli/undeploy'))
   .command(require('./src/cli/list-deployed'))
   .command(require('./src/cli/new-destination'))
