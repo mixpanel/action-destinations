1a832074fe0dc74e1ce261d8c37fbb442b600854
Matt Shwery <matthew.shwery@segment.com>
2020-11-16 18:30:00 -0600

    remove airtable (#80)

--- src/destinations/index.ts
+++ src/destinations/index.ts
@@ -1,5 +1,4 @@
 import { Destination, DestinationDefinition } from '../lib/destination-kit'
-import airtable from './airtable'
 import amplitude from './amplitude'
 import customerio from './customerio'
 import pipedrive from './pipedrive'
@@ -7,7 +6,6 @@ import sendgrid from './sendgrid'
 import slack from './slack'
 
 const destinations: Record<string, DestinationDefinition<any>> = {
-  airtable,
   amplitude,
   customerio,
   pipedrive,
@@ -16,7 +14,6 @@ const destinations: Record<string, DestinationDefinition<any>> = {
 }
 
 const idToSlug: Record<string, string> = {
-  '5f7dd5e61ad74fefa2b1fc46': 'airtable',
   '5f7dd6d21ad74f3842b1fc47': 'amplitude',
   '5f7dd78fe27ce7ff2b8bfa37': 'customerio',
   '5f7dd8191ad74f868ab1fc48': 'pipedrive',
