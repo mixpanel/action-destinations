d7d84c0a1d61a3042d31e8169c84e16c7de9a658
Steven Miller <stevenmiller888@gmail.com>
2021-02-02 14:30:27 -0500

    Destinations: delete sendgrid (#121)

--- packages/destination-actions/src/destinations/index.ts
+++ packages/destination-actions/src/destinations/index.ts
@@ -2,7 +2,6 @@ import { Destination, DestinationDefinition } from '../lib/destination-kit'
 import amplitude from './amplitude'
 import customerio from './customerio'
 import pipedrive from './pipedrive'
-import sendgrid from './sendgrid'
 import slack from './slack'
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -10,7 +9,6 @@ export const destinations: Record<string, DestinationDefinition<any>> = {
   amplitude,
   customerio,
   pipedrive,
-  sendgrid,
   slack
 }
 
@@ -18,7 +16,6 @@ export const idToSlug: Record<string, string> = {
   '5f7dd6d21ad74f3842b1fc47': 'amplitude',
   '5f7dd78fe27ce7ff2b8bfa37': 'customerio',
   '5f7dd8191ad74f868ab1fc48': 'pipedrive',
-  '5f7dd8848e9d93aafb6cecd8': 'sendgrid',
   '5f7dd8e302173ff732db5cc4': 'slack'
 }
 
