8e67da19f14e2a0b2959e8e52ba42194c3725127
Matt Shwery <matthew.shwery@segment.com>
2021-03-23 16:04:18 -0500

    remove fab-5-engine treb deploy (#170)

--- scripts/run-ops.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Allow the service to run without chamber (for CI, docker-compose, etc)
-if [ -z "$NO_CHAMBER" ];then
-  exec chamber exec fab-5-ops -- node packages/server/dist/src/ops/server.js
-else
-  exec node packages/server/dist/src/ops/server.js
-fi;
