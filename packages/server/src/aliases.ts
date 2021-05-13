cec9028a7c168b9f69c07a7174cc25c194e8fa0a
Matt Shwery <matthew.shwery@segment.com>
2021-05-13 17:42:33 -0500

    run dev server without building (#204)

--- packages/server/src/aliases.ts
+++ packages/server/src/aliases.ts
@@ -1,4 +1,12 @@
-import moduleAlias from 'module-alias'
+import path from 'path'
+import { register } from 'tsconfig-paths'
+import devConfig from '../tsconfig.json'
+import prodConfig from '../tsconfig.build.json'
+import { NODE_ENV } from './config'
 
-moduleAlias.addAlias('@', __dirname)
-moduleAlias()
+const tsconfig = NODE_ENV === 'production' ? prodConfig : devConfig
+
+register({
+  baseUrl: path.join(__dirname, '../'),
+  paths: tsconfig.compilerOptions.paths
+})
