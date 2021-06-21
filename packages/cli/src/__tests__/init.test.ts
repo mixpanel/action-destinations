a300643dafb033ae3bbd2e19b12eb46b8f87cd0a
lcampos <lcamposguajardo@gmail.com>
2021-06-21 09:41:32 -0700

    Add tests for init cli command (#242)

---
+++ packages/cli/src/__tests__/init.test.ts
@@ -0,0 +1,61 @@
+import { test } from '@oclif/test'
+import * as fs from 'fs'
+import * as path from 'path'
+import * as prompt from '../lib/prompt'
+import * as rimraf from 'rimraf'
+
+describe('cli init', () => {
+  const testDir = path.join('.', 'testResults')
+  beforeAll(() => {
+    if (!fs.existsSync(testDir)) {
+      fs.mkdir(testDir, (err) => {
+        console.log(err)
+      })
+    }
+  })
+
+  afterAll(() => {
+    if (fs.existsSync(testDir)) {
+      rimraf.sync(testDir)
+    }
+  })
+
+  test
+    .stub(prompt, 'autoPrompt', () => {
+      return { directory: testDir, name: 'test basic', slug: 'test-basic', template: 'basic-auth' }
+    })
+    .stdout()
+    .command(['init'])
+    .it('should scaffold an action with basic auth scheme', (ctx) => {
+      console.log(ctx.stdout)
+      expect(ctx.stdout).toContain('Done creating "test basic"')
+      const scaffoldedAction = fs.readFileSync(path.join(testDir, 'test-basic', 'index.ts'), 'utf8')
+      expect(scaffoldedAction).toContain("scheme: 'basic'")
+    })
+
+  test
+    .stub(prompt, 'autoPrompt', () => {
+      return { directory: testDir, name: 'test custom auth', slug: 'test-custom-auth', template: 'custom-auth' }
+    })
+    .stdout()
+    .command(['init'])
+    .it('should scaffold an action with custom auth scheme', (ctx) => {
+      console.log(ctx.stdout)
+      expect(ctx.stdout).toContain('Done creating "test custom auth"')
+      const scaffoldedAction = fs.readFileSync(path.join(testDir, 'test-custom-auth', 'index.ts'), 'utf8')
+      expect(scaffoldedAction).toContain("scheme: 'custom'")
+    })
+
+  test
+    .stub(prompt, 'autoPrompt', () => {
+      return { directory: testDir, name: 'test minimal', slug: 'test-minimal', template: 'minimal' }
+    })
+    .stdout()
+    .command(['init'])
+    .it('should scaffold a minimal action', (ctx) => {
+      console.log(ctx.stdout)
+      expect(ctx.stdout).toContain('Done creating "test minimal"')
+      const scaffoldedAction = fs.readFileSync(path.join(testDir, 'test-minimal', 'index.ts'), 'utf8')
+      expect(scaffoldedAction).toContain("name: 'test minimal'")
+    })
+})
