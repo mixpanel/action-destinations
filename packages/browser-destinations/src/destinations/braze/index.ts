18230bd470c941fde47396b7992508d8fc9c023c
Netto Farah <nettofarah+github@gmail.com>
2021-07-30 16:52:57 -0500

    web: Properly load braze SDK (#311)
    * Properly load braze SDK
    
    * Remove init script
    
    * Patch CLI tests
    
    * Backfill window in oclif

--- packages/browser-destinations/src/destinations/braze/index.ts
+++ packages/browser-destinations/src/destinations/braze/index.ts
@@ -1,8 +1,7 @@
 import type { Settings } from './generated-types'
 import type { BrowserDestinationDefinition } from '../../lib/browser-destinations'
 import { browserDestination } from '../../runtime/shim'
-import { initScript } from './init-script'
-import type appboy from '@braze/web-sdk'
+import appboy from '@braze/web-sdk'
 import logCustomEvent from './logCustomEvent'
 import logUser from './logUser'
 
@@ -10,29 +9,6 @@ declare global {
   interface Window {
     appboy: typeof appboy
   }
-}
-
-interface BrazeInitializeSettings {
-  apiKey: string
-  safariWebsitePushId: string
-  allowCrawlerActivity: boolean
-  doNotLoadFontAwesome: boolean
-  enableLogging: boolean
-  automaticallyDisplayMessages: boolean
-  localization: string
-  minimumIntervalBetweenTriggerActionsInSeconds: number
-  openInAppMessagesInNewTab: boolean
-  openNewsFeedCardsInNewTab: boolean
-  sessionTimeoutInSeconds: number
-  requireExplicitInAppMessageDismissal: boolean
-  enableHtmlInAppMessages: boolean
-  trackAllPages: boolean
-  trackNamedPages: boolean
-  customEndpoint: string
-  version: number
-  logPurchaseWhenRevenuePresent: boolean
-  onlyTrackKnownUsersOnWeb: boolean
-  baseUrl: string
 }
 
 // Switch from unknown to the partner SDK client types
@@ -61,40 +37,37 @@ export const destination: BrowserDestinationDefinition<Settings, typeof appboy> 
   initialize: async ({ settings }, dependencies) => {
     // default options set at the legacy appboy destination
     // not sure if this is needed yet
-    const config: BrazeInitializeSettings = {
-      apiKey: settings.api_key,
+    const config: appboy.InitializationOptions = {
       safariWebsitePushId: '',
       allowCrawlerActivity: false,
       doNotLoadFontAwesome: false,
       enableLogging: false,
-      automaticallyDisplayMessages: true,
       localization: 'en',
       minimumIntervalBetweenTriggerActionsInSeconds: 30,
       openInAppMessagesInNewTab: false,
-      openNewsFeedCardsInNewTab: false,
       sessionTimeoutInSeconds: 30,
       requireExplicitInAppMessageDismissal: false,
       enableHtmlInAppMessages: false,
-      trackAllPages: false,
-      trackNamedPages: false,
-      customEndpoint: '',
+      // openNewsFeedCardsInNewTab: false,
+      // automaticallyDisplayMessages: true,
+      // trackAllPages: false,
+      // trackNamedPages: false,
+      // customEndpoint: '',
       // changed from 1 to 3
-      version: 3,
-      logPurchaseWhenRevenuePresent: false,
-      onlyTrackKnownUsersOnWeb: false,
+      // version: 3,
+      // logPurchaseWhenRevenuePresent: false,
+      // onlyTrackKnownUsersOnWeb: false,
       baseUrl: settings.endpoint
     }
 
-    initScript({
-      apiKey: settings.api_key,
-      config
-    })
-
-    if (dependencies) {
-      await dependencies.loadScript('https://js.appboycdn.com/web-sdk/3.3/service-worker.js')
-      await dependencies.resolveWhen(() => Object.prototype.hasOwnProperty.call(window, 'appboy'), 100)
+    await dependencies?.loadScript('https://js.appboycdn.com/web-sdk/3.3/service-worker.js')
+    const initialized = appboy.initialize(settings.api_key, config)
+    if (!initialized) {
+      throw new Error('Failed to initialize AppBoy')
     }
-    return window.appboy
+
+    appboy.openSession()
+    return appboy
   },
 
   actions: {
